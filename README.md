36abd37836e5d3c01e4e017f3c6e8f9a8ba3d1b7
Maya Koneval <mayakoneval@Mayas-MBP-2.localdomain>
2018-08-15 14:07:21 -0700

    typescript undefined rule

---
+++ README.md
@@ -0,0 +1,51 @@
+# eslint-plugin-maya
+
+later
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-maya`:
+
+```
+$ npm install eslint-plugin-maya --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-maya` globally.
+
+## Usage
+
+Add `maya` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "maya"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "maya/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/tsx-unnecessary-undefined.js
@@ -0,0 +1,93 @@
+/**
+ * @fileoverview &#34;or&#34; undefined type declarations in typescript don&#39;t pass, since all types are &#34;or&#34; undefined
+ * @author Maya
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/tsx-unnecessary-undefined"),
+  RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("tsx-unnecessary-undefined", rule, {
+  valid: [
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = { Moon: string; }"
+    },
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = { Moon: string | undefined; }"
+    },
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = { Moon?: string; }"
+    },
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = { Venus: Planet; }"
+    },
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = string;"
+    },
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = { Moon: string; Sun: Moon; }"
+    },
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = { Moon: string; Sun: Moon | undefined; }"
+    }
+  ],
+
+  invalid: [
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = { Moon?: string | undefined; };",
+      errors: [
+        {
+          message: "Optional types already are optionally undefined"
+        }
+      ]
+    },
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = { Moon?: string | undefined; Sun: string; };",
+      errors: [
+        {
+          message: "Optional types already are optionally undefined"
+        }
+      ]
+    },
+    {
+      parser: "typescript-eslint-parser",
+      code: "type Star = { Moon?: string; Sun?: string | undefined; };",
+      errors: [
+        {
+          message: "Optional types already are optionally undefined"
+        }
+      ]
+    },
+    {
+      parser: "typescript-eslint-parser",
+      code:
+        "type Star = { Moon?: string | undefined; Sun?: string | undefined; };",
+      errors: [
+        {
+          message: "Optional types already are optionally undefined"
+        },
+        {
+          message: "Optional types already are optionally undefined"
+        }
+      ]
+    }
+  ]
+});
